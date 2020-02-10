b21fc26b3409bb5680d38bea8cc9df49091ce08e
John Wolfe <jwolfe890@gmail.com>
2020-02-09 22:19:14 -0600

    configured for common prod dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,44 +1,38 @@
 const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: {
-    main: "./src/main.js"
-  },
+module.exports = merge(common, {
   mode: "development",
   output: {
-    // content hashing is used here to prevent browsers from cachine the bunlde build
-    filename: "main.[contentHash].js",
+    // the dev build doesn't have a hash
+    filename: "main.js",
     path: path.resolve(__dirname, "dist")
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.html$/,
-        use: ["html-loader"]
-      },
-      {
-        test: /\.jpg$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "imgs",
-              esModule: false
-            }
-          }
-        ]
-      }
-    ]
   }
-};
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       use: ["style-loader", "css-loader"]
+  //     },
+  //     {
+  //       test: /\.html$/,
+  //       use: ["html-loader"]
+  //     },
+  //     {
+  //       test: /\.jpg$/,
+  //       use: [
+  //         {
+  //           loader: "file-loader",
+  //           options: {
+  //             name: "[name].[ext]",
+  //             outputPath: "imgs",
+  //             esModule: false
+  //           }
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
+});
