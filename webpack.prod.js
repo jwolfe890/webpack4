80dd1afa03122ab184ccfcead4797f3c0aa85767
John Wolfe <jwolfe890@gmail.com>
2020-02-10 00:38:38 -0600

    added clean webpack plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -9,30 +9,6 @@ module.exports = merge(common, {
     // content hashing is used here to prevent browsers from cachine the bunlde build
     filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
-  }
-  //   module: {
-  //     rules: [
-  //       {
-  //         test: /\.css$/,
-  //         use: ["style-loader", "css-loader"]
-  //       },
-  //       {
-  //         test: /\.html$/,
-  //         use: ["html-loader"]
-  //       },
-  //       {
-  //         test: /\.jpg$/,
-  //         use: [
-  //           {
-  //             loader: "file-loader",
-  //             options: {
-  //               name: "[name].[ext]",
-  //               outputPath: "imgs",
-  //               esModule: false
-  //             }
-  //           }
-  //         ]
-  //       }
-  //     ]
-  //   }
+  },
+  plugins: [new CleanWebpackPlugin()]
 });
